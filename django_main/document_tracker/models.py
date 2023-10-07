06a93984639a068bd427ccdd0dfdb3b62028a4b2
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2023-10-07 13:31:43 +0800

    Added database schema
    Following the django tutorial on their website
    
    This encompasses steps 1-2 with edits to more closely align with our project

--- django_main/document_tracker/models.py
+++ django_main/document_tracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    file_content = models.FileField(upload_to="uploads/")
+    created_date = models.DateTimeField("date created")
+    last_modified_date = models.DateTimeField("date last modified")
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    color = models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.title_text
