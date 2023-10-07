1307eb9d43ade9931085bbae650e3697b96c9123
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2023-10-07 22:18:21 +0800

    initial implementation of the tag system
    still need to do the file forms too

--- django_main/document_tracker/views.py
+++ django_main/document_tracker/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
-from .models import File
+
+from .models import File, Tag
 
 
 def index(request):
@@ -13,4 +16,15 @@ def index(request):
 
 def detail(request, file_id):
     file = get_object_or_404(File, pk=file_id)
+    
     return render(request, "document_tracker/detail.html", {"file": file})
+
+
+def add_tag(request, file_id):
+    # TODO error handling
+    # TODO http response
+    
+    file = get_object_or_404(File, pk=file_id)
+    Tag.objects.create(file=file, title=request.POST["tag_name"], color=request.POST["tag_color"])
+
+    return HttpResponseRedirect("/")
